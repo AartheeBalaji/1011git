54d9d31e48405eb6b6e82dce8ac2632c7d8e559c
1011git <aartheegunasekar@gmail.com>
2021-11-10 15:25:40 +0530

    mytest

---
+++ src/org/test/Class.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Class {
+	private void empname() {
+    System.out.println("aarthi");
+	}
+	public static void main(String[] args) {
+		Class a = new Class();
+		a.empname();
+	}
+
+}
