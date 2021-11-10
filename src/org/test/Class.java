56335bc5b501591758b586d2b9d43bcfda9dbb50
1011git <aartheegunasekar@gmail.com>
2021-11-10 15:46:50 +0530

    testswetha

--- src/org/test/Class.java
+++ src/org/test/Class.java
@@ -3,7 +3,10 @@ package org.test;
 public class Class {
 	private void empname() {
     System.out.println("aarthi");
-	}
+    }
+	private void empadd() {
+		System.out.println("chennai");
+		}
 	public static void main(String[] args) {
 		Class a = new Class();
 		a.empname();
